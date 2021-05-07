1de9a043f906d7e3a66c25ae9e25b7bb6b39326e
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2021-05-07 15:09:10 -0300

    Network image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://image.freepik.com/fotos-gratis/diamond-3d-render_103577-5774.jpg'),
+          ),
+        ),
       ),
     ),
   );

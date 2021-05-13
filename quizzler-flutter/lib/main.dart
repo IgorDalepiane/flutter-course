e68dc508e3fe8c110d79c74cdd19d80e386ba81a
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2021-05-13 11:31:37 -0300

    Questions list, increase index

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -28,24 +28,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    )
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int questionIndex = 0;
+
+  void increaseIndex() {
+    setState(() {
+      if (questionIndex + 1 != questions.length) {
+        questionIndex++;
+      } else {
+        questionIndex = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -83,12 +82,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ));
-                });
+                increaseIndex();
               },
             ),
           ),
@@ -105,7 +99,9 @@ class _QuizPageState extends State<QuizPage> {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                increaseIndex();
+              },
             ),
           ),
         ),

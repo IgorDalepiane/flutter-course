6e71eea3698ea21240b272d2e58ba5c708b9b8e1
Igor Dalepiane da Costa <igor.costa@zallpy.com>
2021-05-19 11:35:17 -0300

    Clima http package

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -21,8 +21,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
+// ?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'
+  void getData() async {
+    http.Response response = await http.get(Uri.https('samples.openweathermap.org', '/data/2.5/weather',
+        {"lat": "35", "lon": "139", "appid": "b6907d289e10d714a6e88b30761fae22"}));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.body);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
